1e49e151f2c99c817c7fb5cbeffd07e5a2311a90
DiegoxBsB <dgomesxbsb@gmail.com>
2025-02-28 23:04:36 +0000

    Versão FInal

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@
 int main() {
 
    
-char estado1[20], estado2[20];
+char estado1[20], estado2[20], codigo1[10], codigo2[10], cidade1 [20], cidade2[20];
 int populacao1, populacao2, pturis1, pturis2;
 float area1, area2;
 float pib1, pib2;
@@ -13,6 +13,12 @@ float pib1, pib2;
 
 printf ("Digite o nome do estado:\n");
 scanf ("%s", &estado1);
+
+printf ("Digite o código da carta: \n");
+scanf ("%s", &codigo1);
+
+printf ("Digite o nome da cidade: \n");
+scanf ("%s", &cidade1);
 
 printf ("Digite a população:\n");
 scanf ("%d", &populacao1);
@@ -29,6 +35,8 @@ scanf ("%d", &pturis1);
 //Declarando as características do primeiro estado na saida.
 
 printf ("O estado é: %s \n", estado1);
+printf ("O código é: %s \n", codigo1);
+printf ("A cidade é: %s \n", cidade1);
 printf ("A populção é: %d \n", populacao1);
 printf ("A área é de: %.3f KM² \n", area1);
 printf ("O PIB é: %.3f \n" , pib1);
@@ -41,6 +49,12 @@ printf ("O estado tem: %d pontos turísticos \n", pturis1);
 
 printf ("Digite o nome do segundo estado:\n");
 scanf ("%s", &estado2);
+
+printf ("Digite o código da carta: \n");
+scanf ("%s", &codigo2);
+
+printf ("Digite o nome da cidade: \n");
+scanf ("%s", &cidade2);
 
 printf ("Digite a população:\n");
 scanf ("%d", &populacao2);
@@ -59,6 +73,8 @@ scanf ("%d", &pturis2);
 
 
 printf ("O estado é: %s \n", estado2);
+printf ("O código é: %s \n", codigo2);
+printf ("A cidade é: %s \n", cidade2);
 printf ("A populção é: %d \n", populacao2);
 printf ("A área é de: %.3f KM² \n", area2);
 printf ("O PIB é: %.3f \n" , pib2);
